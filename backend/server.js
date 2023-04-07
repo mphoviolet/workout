8b402527cde485b86ed814f347553c948eedc4c3
mpho97 <mphotema97@gmail.com>
2023-04-07 10:21:07 +0200

    edit commit

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
-//const cors = require("cors");
+const cors = require("cors");
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 
 const app = express()
-//app.use(cors());
+app.use(cors());
 app.use(express.json())
 
 app.use((req, res, next) =>{
